ba1e4419a37c5ec5571ab2e0573a38a24b8e671d
Laszlo Roovers <l.roovers@student.tudelft.nl>
2025-05-04 17:41:39 +0200

    Implemented training pipeline

---
+++ src/sentiment-analysis/train.py
@@ -0,0 +1,42 @@
+# General imports
+import os
+from joblib import dump
+
+# ML imports
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import confusion_matrix, accuracy_score
+
+def train(file="/data/RestaurantReviews_HistoricDump.tsv", max_features=1420, test_size=0.20, seed=42):
+
+    # Needs to be connected through model-service to ml-lib for the preprocessing
+    X, y = ml_lib.preprocess(file)
+    cv = CountVectorizer(max_features=max_features)
+    X = cv.fit_transform(X).toarray()
+    
+    # Split data into train and test set
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
+
+    # Train classifier
+    model = GaussianNB()
+    model.fit(X_train, y_train)
+
+    # Predict on test set
+    y_pred =  model.predict(X_test)
+
+    # Evaluate performance
+    cm  = confusion_matrix(y_test, y_pred)
+    acc = accuracy_score(y_test, y_pred)
+    print(cm)
+    print(acc)
+
+    # Save BoW dictionary & trained model
+    os.makedirs("model", exist_ok=True)
+    dump(cv, "model/bow.pkl")
+    dump(model, "model/model.pkl")
+
+
+
+if __name__ == "__main__":
+    train()    
