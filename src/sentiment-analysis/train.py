cae51bf8120d9c90fa80a1acaf02074918f4bd1f
Laszlo Roovers <l.roovers@student.tudelft.nl>
2025-05-04 17:53:07 +0200

    Added docstring

--- src/sentiment-analysis/train.py
+++ src/sentiment-analysis/train.py
@@ -8,13 +8,23 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
 
-def train(file : str ="/data/RestaurantReviews_HistoricDump.tsv", max_features : int = 1420, test_size : float =0.20, seed : int =42) -> None:
+def train(file : str ="/data/RestaurantReviews_HistoricDump.tsv", max_features : int = 1420, test_size : float =0.30, seed : int =42) -> None:
+    """Trains a classifiers for sentimient analysis.
 
-    # Needs to be connected through model-service to ml-lib for the preprocessing
+    Parameters:
+        - filepath: (str), relative path to dataset.
+        - max_features: (int), number of features for CountVectorizer (default = 1420).
+        - test_size: (float), ratio of train/test split (default = 0.30).
+        - seed: (int), seed for reproducibility (default = 42).
+
+    Returns:
+        - None
+    """
+    # TODO: Needs to be connected through model-service to ml-lib for the preprocessing
     X, y = ml_lib.preprocess(file)
     cv = CountVectorizer(max_features=max_features)
     X = cv.fit_transform(X).toarray()
-    
+
     # Split data into train and test set
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
 
@@ -35,7 +45,6 @@ def train(file : str ="/data/RestaurantReviews_HistoricDump.tsv", max_features :
     os.makedirs("model", exist_ok=True)
     dump(cv, "model/bow.pkl")
     dump(model, "model/model.pkl")
-
 
 
 if __name__ == "__main__":
