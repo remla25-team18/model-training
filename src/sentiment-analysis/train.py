71f166bae0e280bf5bcac6ed19eec254e1536214
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-05-05 10:16:33 +0200

    updated train method

--- src/sentiment-analysis/train.py
+++ src/sentiment-analysis/train.py
@@ -1,34 +1,45 @@
 # General imports
 import os
 from joblib import dump
+import datetime
+import pandas as pd
 
 # ML imports
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix, accuracy_score
+from lib_ml.preprocess import preprocess_text
 
-def train(file : str ="/data/RestaurantReviews_HistoricDump.tsv", max_features : int = 1420, test_size : float =0.30, seed : int =42) -> None:
-    """Trains a classifiers for sentimient analysis.
+def train(file: str = "data/RestaurantReviews_HistoricDump.tsv", max_features: int = 1420, test_size: float = 0.30, seed: int = 42):
+    """
+    Trains a sentiment analysis classifier.
 
     Parameters:
-        - filepath: (str), relative path to dataset.
-        - max_features: (int), number of features for CountVectorizer (default = 1420).
-        - test_size: (float), ratio of train/test split (default = 0.30).
-        - seed: (int), seed for reproducibility (default = 42).
+        - file: (str), path to dataset.
+        - max_features: (int), number of features for CountVectorizer.
+        - test_size: (float), train/test split ratio.
+        - seed: (int), random seed.
 
     Returns:
-        - None
+        - tuple: (trained model, vectorizer)
     """
-    # TODO: Needs to be connected through model-service to ml-lib for the preprocessing
-    X, y = ml_lib.preprocess(file)
-    cv = CountVectorizer(max_features=max_features)
-    X = cv.fit_transform(X).toarray()
+    print(os.getcwd())
 
-    # Split data into train and test set
+    # Loading the dataset, preprocessing, splitting
+    print("Loading dataset...")
+    dataset = pd.read_csv(file, delimiter="\t", quoting=3)
+    corpus = preprocess_text(dataset)
+    y = dataset.iloc[:, -1].values
+    
+    print("Processing dataset...")
+    cv = CountVectorizer(max_features=max_features)
+    X = cv.fit_transform(corpus).toarray()
+
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=seed)
 
-    # Train classifier
+    # Train the model
+    print("Training model...")
     model = GaussianNB()
     model.fit(X_train, y_train)
 
@@ -41,11 +52,14 @@ def train(file : str ="/data/RestaurantReviews_HistoricDump.tsv", max_features :
     print(cm)
     print(acc)
 
-    # Save BoW dictionary & trained model
-    os.makedirs("model", exist_ok=True)
-    dump(cv, "model/bow.pkl")
-    dump(model, "model/model.pkl")
+    # Save the model and vectorizer
+    print("Saving model and vectorizer...")
+    now = datetime.datetime.now()
+    timestamped_version = now.strftime("%Y%m%d")
 
+    os.makedirs("tmp",exist_ok=True)
+    dump(model, f"tmp/model-{timestamped_version}.pkl")
+    dump(cv, f"tmp/bow-{timestamped_version}.pkl")
 
 if __name__ == "__main__":
     train()    
