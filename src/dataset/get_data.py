8fb12221fec48fa6454f4af2124a0728f7cd9ed4
Madalena Barroso Gomes Pereira Placido <M.BarrosoGomesPereiraPlacido@student.tudelft.nl>
2025-05-24 00:31:36 +0200

    fixi linter results

--- src/dataset/get_data.py
+++ src/dataset/get_data.py
@@ -1,3 +1,7 @@
+'''
+get_data.py
+'''
+
 from joblib import dump
 import pandas as pd
 from lib_ml.preprocess import preprocess_text
@@ -5,12 +9,17 @@ import gdown
 
 
 def download_file_from_google_drive(training_file_dir):
+    '''
+    Downloads a file from google drive
+    '''
     gdrive_url = "https://drive.google.com/uc?id=12ibFg0ReSlJYkTpPFbOhkzvZ1TN5bciO"
     gdown.download(gdrive_url, training_file_dir, quiet=False)
 
 
 def get_data(training_file_dir, joblib_output_dir):
-    # Loading the dataset
+    '''
+    Loads the dataset
+    '''
     dataset = pd.read_csv(training_file_dir, delimiter="\t", quoting=3)
     corpus = preprocess_text(dataset)
     y = dataset.iloc[:, -1].values
