f12b7ff65de7d8d410eddff28fb6997bef88dc7f
Madalena Barroso Gomes Pereira Placido <M.BarrosoGomesPereiraPlacido@student.tudelft.nl>
2025-05-24 21:42:55 +0200

    non-default flake8 configuration

---
+++ linter_ml_rules/flake8/check_kwarg_dataset_split.py
@@ -0,0 +1,36 @@
+'''
+check_kwarg_dataset_split.py
+'''
+import ast
+
+class DatasetSplitKwargChecker:
+    '''
+    Checks whether additional train_test_split function parameters 
+    (aside from the first two) need to use keyword arguments
+    '''
+    name = "flake8_dataset_split_kwarg_checker"
+
+    def __init__(self, tree):
+        self.tree = tree
+
+    def run(self):
+        '''
+        Informs the user when additional train_test_split function parameters 
+        (aside from the first two) are not using keyword arguments
+        '''
+        for node in ast.walk(self.tree):
+            if isinstance(node, ast.Call):
+                func_name = getattr(node.func, "id", None)
+
+                if func_name == "train_test_split":
+                    allowed_positional = 2 # the first two parameters (X, y) don't need keywords
+                    num_positional = len(node.args)
+
+                    if num_positional > allowed_positional:
+                        # We want to flag when the rest of the parameters don't have keywords (are positional)
+                        yield (
+                            node.lineno,
+                            node.col_offset,
+                            "TTS001 Use keyword arguments for parameters after the first two in train_test_split",
+                            type(self),
+                        )
--- linter_ml_rules/pylint/check_random_state_dataset_split.py
+++ linter_ml_rules/pylint/check_random_state_dataset_split.py
@@ -1,9 +1,15 @@
+'''
+check_random_state_dataset_split.py
+'''
 from pylint.checkers.base_checker import BaseChecker
 import astroid
 
 class DatasetSplitRandomStateChecker(BaseChecker):
+    '''
+    Checks if the train_test_split has a random_state set
+    '''
     name = "dataset-split-random-state"
-    priority = -1 # lower
+    priority = -1 # lower priority
 
     msgs = {
         "W9001": (
@@ -14,11 +20,10 @@ class DatasetSplitRandomStateChecker(BaseChecker):
     }
 
     def visit_call(self, node: astroid.nodes.Call):
+        '''
+        Informs the user when a random_state is not set in train_test_split
+        '''
         if isinstance(node.func, astroid.nodes.Name) and node.func.name == "train_test_split":
             keywords = {kw.arg for kw in node.keywords if kw.arg is not None}
             if "random_state" not in keywords:
                 self.add_message("train-test-split-missing-random-state", node=node)
-
-
-
-
