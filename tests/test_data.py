ab38144e4f83bf179fed5e48b987eba50cf76fce
Yahor Dziomin <y.y.dziomin@student.tudelft.nl>
2025-05-26 20:46:17 +0200

    added test for target feature distribution

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,6 +2,7 @@
 test_data.py
 """
 import pytest
+import numpy as np
 from joblib import load
 
 @pytest.fixture()
@@ -10,23 +11,27 @@ def data_setup():
     Setup function to prepare the environment for testing
     Returns the corpus and labels
     """
-    corpus = load("tmp/corpus.joblib")
+    corpus = load("tmp/corpus_processed.joblib")
     y = load("tmp/y.joblib")
     if corpus is None or y is None:
         raise ValueError("Corpus or labels are not loaded correctly.")
     return corpus, y
 
+@pytest.fixture()
+def data_test_setup():
+    """
+    Additional setup function to get test data
+    """
+    y_test = load("tmp/y_test.joblib")
+    if y_test is None:
+        raise ValueError("Test labels are not loaded correctly.")
+    return y_test
 
-def test_code_is_tested():
-    """
-    dummy function for now
-    """
-    # assert False
-    assert True
 
 def test_correct_format(data_setup):
     """
     Test to ensure there are no missing values in the dataset
+    ML Test Score, Data 4: Features adhere to meta-level requirements
     """
     corpus, y = data_setup
     assert corpus is not None, "Corpus should not be None"
@@ -37,12 +42,39 @@ def test_correct_format(data_setup):
 def test_no_missing_values(data_setup):
     """
     Test to ensure there are no missing values in the dataset
+    ML Test Score, Data 4: Features adhere to meta-level requirements
     """
     corpus, y = data_setup
     # Check for missing values
     for i in range(len(corpus)):
-        # print(f"{i}: {corpus[i]}")
         if not corpus[i]:
             print(f"Missing value found in corpus at index {i}")
     assert all(corpus), "Corpus contains missing values"
-    assert all(y), "Labels contain missing values"
+    assert None not in y, "Labels contain missing values"
+
+def test_labels_in_the_test_distributed_proportionally(data_setup, data_test_setup):
+    """
+    Test to ensure labels are distributed proportionally in the overall and test datasets
+    ML Test Score, Data 1: Feature expectations are captured in a schema
+    """
+    _, y = data_setup
+    y_test = data_test_setup
+    total_count = len(y)
+    total_count_test = len(y_test)
+    unique_labels, counts = np.unique(y, return_counts=True)
+    unique_labels_test, counts_test = np.unique(y_test, return_counts=True)
+    # Check if all labels are present
+    assert len(unique_labels) > 0, "No labels found in the dataset"
+
+    # Check if the labels in the test set are a subset of the overall labels
+    assert set(unique_labels_test).issubset(set(unique_labels)), "Test labels are not a subset of overall labels"
+
+    # Check if the distribution of labels in the overall dataset is not too skewed
+    min_count_prop = min(counts) / total_count
+    print(f"Label proportion difference in overall set: \n{counts / total_count}")
+    assert min_count_prop > 0.2, "Labels are not distributed proportionally in the overall dataset"
+    
+    # Check if the distribution is proportional in the test set
+    count_diff_test = (max(counts_test) - min(counts_test))/ total_count_test
+    print(f"Label proportion difference in test set: \n{counts_test / total_count_test}")
+    assert count_diff_test < 0.2, "Test labels are not distributed proportionally"
